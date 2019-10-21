6bae3e90b35e2f3cbc8768b0b3f59ec6bc3f2593
DanieleMarchei <danielemarchei@gmail.com>
2019-10-21 16:21:09 +0000

    workspace manager

---
+++ WoLaTa Task Manager/Model/WorkspaceManager.cs
@@ -0,0 +1,38 @@
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoLaTa_Task_Manager.Model
+{
+    public static class WorkspaceManager
+    {
+        public static Workspace LoadWorkspace(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string json = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<Workspace>(json);
+            }
+        }
+
+        public static Workspace CreateWorkspace(string label, string path)
+        {
+            Workspace w = new Workspace(label);
+            SaveWorkspace(w, path);
+            return w;
+        }
+
+        public static void SaveWorkspace(Workspace w, string path)
+        {
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                string json = JsonConvert.SerializeObject(w);
+                writer.Write(json);
+            }
+        }
+    }
+}
---
+++ WoLaTa_Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WoLaTa_Task_Manager.Model;
+
+namespace WoLaTa_Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("test");
+            WorkspaceManager.CreateWorkspace("workspaceTest", @"C:\Users\Daniele\Desktop\workspace.json");
+        }
+    }
+}
