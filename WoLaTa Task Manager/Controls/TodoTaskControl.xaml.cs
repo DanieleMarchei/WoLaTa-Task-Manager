9e6f7386318ecc1476f4b3c939a1bc6ede166b0c
Sævar Már Atlason <saevar.mar.atlason@advania.is>
2019-10-24 15:53:42 +0000

    Added WorkspaceWindow, MainWindow functionality and TodoTask control

---
+++ WoLaTa Task Manager/Controls/TodoTaskControl.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WoLaTa_Task_Manager.Model;
+
+namespace WoLaTa_Task_Manager.Controls
+{
+    /// <summary>
+    /// Interaction logic for TodoTaskControl.xaml
+    /// </summary>
+    public partial class TodoTaskControl : UserControl
+    {
+        public TodoTask TodoTask { get; private set; }
+
+        public TodoTaskControl(TodoTask todoTask)
+        {
+            InitializeComponent();
+            this.TodoTask = todoTask;
+        }
+    }
+}
---
+++ WoLaTa Task Manager/Windows/WorkspaceWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WoLaTa_Task_Manager.Model;
+
+namespace WoLaTa_Task_Manager.Windows
+{
+    /// <summary>
+    /// Interaction logic for WorkspaceWindow.xaml
+    /// </summary>
+    public partial class WorkspaceWindow : Window
+    {
+        public Workspace Workspace { get; private set; }
+        public WorkspaceWindow(Workspace workspace)
+        {
+            InitializeComponent();
+
+            this.Workspace = workspace;
+        }
+
+
+    }
+}
